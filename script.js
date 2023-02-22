8e2a10484954a9d59035964f1d328d235254da40
Elena Tykhonova <sivak_elena@ukr.net>
2023-02-22 08:57:57 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+let number1 = +prompt('Введіть перше число:', '0');
+let number2 = +prompt('Введіть друге число:', '0');
+let number3 = +prompt('Введіть третє число:', '0');
+alert((number1 + number2 + number3) / 3);
+
